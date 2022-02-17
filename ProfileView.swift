37eb7db8a5ca0f6a2fc584ccc70e1fedf38b95f4
Grigez <grigez@gmail.com>
2022-02-17 22:02:35 +0300

    Первое ДЗ выполнено, но запускается только белый экран.

---
+++ ProfileView.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileView.swift
+//  Navigation
+//
+//  Created by Дмитрий Григорьев on 17.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    @IBOutlet weak var avatarUserView: UIImageView!
+    
+    
+    
+    @IBOutlet weak var nameUserLabel: UILabel!
+    
+    
+    @IBOutlet weak var dateUserLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var cityUserLabel: UILabel!
+    
+    
+    @IBOutlet weak var textUserView: UITextView!
+    
+    
+    
+}
