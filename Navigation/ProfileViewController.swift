6147d17a01f9955f8bd9b0211ce0d4aa067bf16a
Grigez <grigez@gmail.com>
2022-02-23 15:44:43 +0300

    Готово

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Дмитрий Григорьев on 23.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
