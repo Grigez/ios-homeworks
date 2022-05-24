32cf83659914fd673d943cb0bda616f74c0005e5
Grigez <grigez@gmail.com>
2022-05-24 21:47:17 +0300

    Добавлен файл LoginViewController

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Дмитрий Григорьев on 24.05.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
